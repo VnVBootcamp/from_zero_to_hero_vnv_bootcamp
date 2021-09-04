fb66c17bdfc689509542c1da66cd37996038477c
Jennifer Ramirez <jxr20920@dexcom.com>
2021-09-03 22:50:14 -0700

    Create AdvancedWebComponents.java

---
+++ vnvbootcamp/itj.vnv.bootcamp/src/tests/AdvancedWebComponents.java
@@ -0,0 +1,18 @@
+package tests;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class AdvancedWebComponents {
+    public static void main(String[] args) {
+        //For MacOS
+        System.setProperty("webdriver.chrome.driver","/users/jxr20920/chromedriver");
+
+        //For Windows
+        //System.setProperty("webdriver.chrome.driver","C:\\folder\chromedriver.exe");
+
+        WebDriver chromeDriver = new ChromeDriver();
+
+        String baseUrl = "https://www.globalsqa.com/samplepagetest/";
+        chromeDriver.get(baseUrl);
+
+    }
+}
