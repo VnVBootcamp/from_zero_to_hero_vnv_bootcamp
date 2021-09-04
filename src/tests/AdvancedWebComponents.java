2e506e2d6be177206a5bb29eef9f96fc64bc2240
Jennifer Ramirez <jxr20920@dexcom.com>
2021-09-04 00:00:17 -0700

    Created method to call on main
    Created method to call on main

--- src/tests/AdvancedWebComponents.java
+++ src/tests/AdvancedWebComponents.java
@@ -3,18 +3,22 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class AdvancedWebComponents {
+
+    private static WebDriver chromeDriver = new ChromeDriver();
+
     public static void main(String[] args) {
+        openBrowser();
+    }
+
+    private static void openBrowser(){
         //For MacOS
         System.setProperty("webdriver.chrome.driver","/users/jxr20920/chromedriver");
 
         //For Windows
         //System.setProperty("webdriver.chrome.driver","C:\\folder\chromedriver.exe");
 
-        WebDriver chromeDriver = new ChromeDriver();
-
         String baseUrl = "https://www.globalsqa.com/samplepagetest/";
         chromeDriver.get(baseUrl);
-
     }
 }
 
